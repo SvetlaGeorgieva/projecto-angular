ff211a5b9a2e7b4e5e7b2f65e55af2087a75dad3
SvetlaGeorgieva <sv.georgieva@gmail.com>
2022-02-10 22:41:56 +0200

    Project details page: - list tasks per project - show task's title and status

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Project, projects } from '../projects';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
-  styleUrls: ['./project-details.component.css']
+  styleUrls: ['./project-details.component.css'],
 })
 export class ProjectDetailsComponent implements OnInit {
+  project: Project | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-  }
+    // Get the project name from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const projectNameFromRoute = routeParams.get('projectName');
 
+    // Find the project that correspond with the name provided in the route.
+    this.project = projects.find(
+      (project) => project.name === projectNameFromRoute
+    );
+  }
 }
